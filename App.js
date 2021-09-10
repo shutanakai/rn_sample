8dbc1a58558b5777f43838842ed6e54522fb8e80
nakai <shuta.nakai@hakuhodo-ms.co.jp>
2021-09-10 17:44:56 +0900

    fix

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
-import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 
 import ContextProvider from './src/ContextProvider';
